d0308a1e2310fcc498d0f818f63caddede3274aa
ASUS <m>
2021-07-28 23:04:31 +0200

    Replaced observer code in UI Controllers for GameFragment and ScoreFragment with binding expressions in the layout files for the text attributes. In order for this to work the binding object needs to receive the corresponding fragment as the lifecycle owner in the UI Controller. Formatted strings which are defined in the strings.xml file are used in the binding expressions to format the data from the ViewModels.

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -56,14 +56,11 @@ class GameFragment : Fragment() {
         // data in the ViewModel
         binding.gameViewModel = viewModel
 
-        /** Setting up LiveData observation relationship **/
-        viewModel.word.observe(viewLifecycleOwner, Observer { newWord ->
-            binding.wordText.text = newWord
-        })
+        // Specify the current activity as the lifecycle owner of the binding.
+        // This is used so that the binding can observe LiveData updates.
+        binding.lifecycleOwner = this
 
-        viewModel.score.observe(viewLifecycleOwner, Observer { newScore ->
-            binding.scoreText.text = newScore.toString()
-        })
+        /** Setting up LiveData observation relationship **/
 
         viewModel.currentTime.observe(viewLifecycleOwner, Observer { newTime ->
             binding.timerText.text = DateUtils.formatElapsedTime(newTime)
