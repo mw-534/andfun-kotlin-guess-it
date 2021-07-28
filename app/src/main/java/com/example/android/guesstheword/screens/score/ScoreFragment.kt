3dc3327177c32a2b1d8e0c092d3fc2a2de0e9c89
ASUS <m>
2021-07-28 22:33:51 +0200

    Bound ViewModel in <data> element of layout files for GameFragment and ScoreFragment to bind method calls of ViewModels directly to the layout instead of setting up onClickListeners in UI Controller classes. For this to work inside the UI Controller the ViewModel must be passed to corresponding ViewModel property of the binding object. The ViewModel property of the binding object is created through the variable declared in the <data> element of the layout file.

--- app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
@@ -57,12 +57,12 @@ class ScoreFragment : Fragment() {
         viewModelFactory = ScoreViewModelFactory(scoreFragmentArgs.score)
         viewModel = ViewModelProvider(this, viewModelFactory).get(ScoreViewModel::class.java)
 
+        binding.scoreViewModel = viewModel
+
         // Add observer for score
         viewModel.score.observe(viewLifecycleOwner, Observer { newScore ->
                 binding.scoreText.text = newScore.toString()
         })
-
-        binding.playAgainButton.setOnClickListener { viewModel.onPlayAgain() }
 
         // Navigates back to title when button is pressed
         viewModel.eventPlayAgain.observe(viewLifecycleOwner, Observer { playAgain ->
